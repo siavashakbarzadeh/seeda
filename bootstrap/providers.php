069bf8aafa9653610c8d630ef313bd25409224f5
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2026-02-18 19:14:07 +0100

    a

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,12 +1,6 @@
 <?php
 
-use Illuminate\Foundation\Application;
-
-/*
-|--------------------------------------------------------------------------
-| Create The Application
-|--------------------------------------------------------------------------
-*/
-$app = Application::configure(basePath: dirname(__DIR__))->create();
-
-return $app;
+return [
+    App\Providers\AppServiceProvider::class,
+    App\Providers\Filament\AdminPanelProvider::class,
+];
